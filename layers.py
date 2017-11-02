a43c6e426830d219e6fb1347c899f382e7c70988
Pranoot <pphatwar1995@gmail.com>
2017-11-03 02:59:49 +0530

    code

--- layers.py
+++ layers.py
@@ -10,7 +10,6 @@ class PrimaryCaps():
 
 		self.num_capsules = num_capsules  # 32
 		self.capsule_output_vec_dim = capsule_output_vec_dim  # 8
-		print num_capsules, capsule_output_vec_dim
 
 	def __call__(self, prev_layer, filter_size, stride, padding): # input_dim = [batch, 20,20, 256]
  		self.prev_layer = prev_layer  # [batch, 20, 20, 256] or its conv1
@@ -22,8 +21,8 @@ class PrimaryCaps():
 
 		for i in range(self.capsule_output_vec_dim):	
 			# [batch, 6, 6, 32] 
-			with tf.variable_scope('PrmaryCapsuleUnit_' + str(i)):
-				cap = tf.contrib.layers.conv2d(prev_layer, self.num_outputs, self.filter_size, self.stride, self.padding)
+			with tf.variable_scope('PrimaryCapsuleUnit_' + str(i)):
+				cap = tf.contrib.layers.conv2d(prev_layer, self.num_capsules, self.filter_size, self.stride, self.padding)
 				cap = tf.reshape(cap, shape=(None, -1, 1, 1)) #batchsize input error possible [batch, 1152, 1, 1]
 				capsules.append(cap)
 		capsules = squashing(tf.concat(capsules, axis=2)) # axis = 2
@@ -41,14 +40,25 @@ class DigitCaps():
  		self.prev_layer = prev_layer 
 
 		capsules = []
+		prev_layer_shape = prev_layer.get_shape()
+		# print prev_layer_shape
+		# input = tf.reshape(prev_layer, shape=(None, 1152, 1, 8, 1))
 
-		
+  #           with tf.variable_scope('routing'):
+  #               # b_IJ: [1, 1, num_caps_l, num_caps_l_plus_1, 1]
+  #               b_IJ = tf.zeros(shape=[1, 1152, 10, 1, 1], dtype=np.float32)
+  #               capsules = routing(self.input, b_IJ)
+        capsules = tf.squeeze(capsules, axis=1)
 		capsules = squashing(tf.concat(capsules, axis=2)) # axis = 2
 
 		return capsules
 
 def routing():
-	self.b = tf.var
+	# w
+	w = tf.get_variable('Weight', shape=(1, 1152, 10, 8, 16), dtype=tf.float32, initializer=tf.random_normal_initializer(stddev=0.2))
+	
+
+
 
 
 
@@ -56,4 +66,4 @@ def MarginalLoss(y, y_hat):
 	Lc = y * tf.square(tf.maximum(0., 0.9 - y_hat)) + 0.5 * (1 - y) * tf.square(tf.maximum(0., y_hat - 0.1))
 	return tf.reduced_mean(tf.reduced_sum(Lc, axis =1))
 
-a = PrimaryCaps(4,5)
+
