53747da75847a4b3f735860c2753cf90c8eea6f9
Pranoot <pphatwar1995@gmail.com>
2017-11-03 03:01:36 +0530

    code

--- data.py
+++ data.py
@@ -2,6 +2,3 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=False)
-print (mnist)
-x,y= mnist.train.next_batch(12)
-print y
